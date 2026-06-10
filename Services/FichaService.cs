36a050e6e117c26fafb79f894f8dfe18526401a9
JhonLu1821 <jhonlunatorres@gmail.com>
2026-06-10 08:37:11 -0500

    Ultimos detalles

---
+++ Services/FichaService.cs
@@ -0,0 +1,37 @@
+﻿using EventDesk_2._0._0.Data;
+using EventDesk_2._0._0.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventDesk_2._0._0.Services
+{
+    public class FichaService
+    {
+        private readonly IMongoCollection<Ficha> _fichas;
+
+        public FichaService()
+        {
+            var conexion = new Conexion();
+            _fichas = conexion.Fichas;
+        }
+
+        public void Crear(Ficha ficha)
+        {
+            _fichas.InsertOne(ficha);
+        }
+
+        public List<Ficha> ObtenerTodas()
+        {
+            return _fichas.Find(_ => true).ToList();
+        }
+
+        public void Eliminar(ObjectId id)
+        {
+            _fichas.DeleteOne(x => x.Id == id);
+        }
+    }
+}
